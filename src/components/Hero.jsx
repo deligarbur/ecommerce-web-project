930571017c344320ea01e2481cc3818cfd5f0fd2
Delia Garcia <deliagarciaburgos@gmail.com>
2026-09-07 13:43:36 +0200

    feat: update hero image and global styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,15 @@
+import { Link } from 'react-router-dom';
 import './Hero.css';
 export function Hero() {
   return (
     <section className="hero">
-      <h1 className="hero__title">STORE</h1>
+      <div className="hero__content">
+        <h1 className="hero__title">STORE</h1>
+
+        <Link to="/catalog" className="hero__link">
+          Ver colección
+        </Link>
+      </div>
     </section>
   );
 }
